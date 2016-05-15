3207d1c891c0b7c5eb0e83ee421fb1d4afd48d39
Robbie Rosati <robbierosati@gmail.com>
2016-05-15 17:41:31 -0500

    properly sums up flux, doesn't yet properly get fiber radius

--- astrometry/integrate.py
+++ astrometry/integrate.py
@@ -2,36 +2,61 @@ from astropy.io import fits
 from astropy import wcs
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.interpolation import map_coordinates,zoom
 
-with open('../greg/ifuPos.txt','r') as f:
-    f.readline()
-    fiber_dia = float(f.readline())
-    fiber_ra , fiber_dec = [],[]
-    for line in f:
-        a,b = line.split()
-        fiber_ra.append(float(a))
-        fiber_dec.append(float(b))
-with fits.open("sdss2.fits") as h:
-    img_data = h[0].data
-    w = wcs.WCS(h[0].header)
+'''
+Given a fits image and fiber positions and diameters,
+this function will return the integrated flux through each fiber.
 
-pixcrd = w.wcs_world2pix(np.array(zip(fiber_ra,fiber_dec),dtype='float64'),1)
+'''
+def get_flux(sdss_fits_fname,fiber_ra,fiber_dec,fiber_dia,zoom_factor,errors=False) :
 
-#plt.scatter(pixcrd[:,0],pixcrd[:,1])
-#plt.show()
-#img_data[np.round(pixcrd).astype(np.int)] = 0x000000
+    with fits.open(sdss_fits_fname) as h:
+        img_data = h[0].data
+        w = wcs.WCS(h[0].header)
+    pixcrd = w.wcs_world2pix(np.array(zip(fiber_ra,fiber_dec),dtype='float64'),1)
 
-#plt.imshow(img_data,interpolation='nearest',cmap='Greys_r',origin='lower')
-#plt.show()
+    #plt.scatter(pixcrd[:,0],pixcrd[:,1])
+    #plt.show()
+    #img_data[np.round(pixcrd).astype(np.int)] = 0x000000
 
-fiber_r_pix = w.wcs_world2pix([[fiber_ra[0]+fiber_dia,fiber_dec[0]]],1)
-fiber_r_pix -= w.wcs_world2pix([[fiber_ra[0],fiber_dec[0]]],1)
-fiber_r_pix = 1 # TODO fix this!!!
+    #plt.imshow(img_data,interpolation='nearest',cmap='Greys_r',origin='lower')
+    #plt.show()
 
-# take sum over each fiber's pixels.
-fibsum = np.zeros(pixcrd.shape[0])
-# should also take fiber_r into account somehow. At the moment fiber_r is ~1 so this kind of works
-map_coordinates(img_data,pixcrd.T,fibsum,order=3, mode='nearest')
+    rp = w.wcs_world2pix([[fiber_ra[0]+fiber_dia,fiber_dec[0]]],1)
+    rp -= w.wcs_world2pix([[fiber_ra[0],fiber_dec[0]]],1)
+    rp = 1 # TODO fix this!!!
 
-print(fibsum)
+    # we'll take a sum over each fiber's pixels.
+    fibsum = np.zeros(pixcrd.shape[0])
+    if errors:
+        fiberr = np.zeros(pixcrd.shape[0])
+
+    zoomed_img = zoom(input=img_data,zoom=zoom_factor,order=1)
+    effr = zoom_factor*rp-0.5
+    nx,ny = zoomed_img.shape
+
+    for i in range(pixcrd.shape[0]):
+        a,b = zoom_factor*pixcrd[i]
+        # take a circular mask around the fiber's center
+        y,x = np.ogrid[-a:nx-a,-b:ny-b]
+        mask = x*x+y*y <= effr*effr
+        fibsum[i] = np.sum(zoomed_img[mask])/(zoom_factor**2)
+        if errors:
+            fiberr[i] = np.abs(fibsum[i] - np.sum(zoomed_img[x*x+y*y <= rp*rp*zoom_factor**2])/zoom_factor**2)
+            # this error is roughly the possible missed flux from pixels on the edge
+    if errors:
+        return fibsum,fiberr
+    else: return fibsum
+
+if __name__ == '__main__':
+    with open('../greg/ifuPos.txt','r') as f:
+        f.readline()
+        fiber_dia = float(f.readline())
+        fiber_ra , fiber_dec = [],[]
+        for line in f:
+            a,b = line.split()
+            fiber_ra.append(float(a))
+            fiber_dec.append(float(b))
+    print("Flux at the first 30 fibers: ")
+    print(get_flux("sdss2.fits",fiber_ra[:30],fiber_dec[:30],fiber_dia,zoom_factor=5,errors=True))
