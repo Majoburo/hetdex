c8bfa3663aba9f5f83a3ae01568230d302a1dd3f
Robbie Rosati <robbierosati@gmail.com>
2016-05-16 15:40:41 -0500

    do the zooming in a WAY more efficient way. only refine part of image actually near fiber

--- astrometry/integrate.py
+++ astrometry/integrate.py
@@ -25,26 +25,38 @@ def get_flux(sdss_fits_fname,fiber_ra,fiber_dec,fiber_dia,zoom_factor,errors=Fal
 
     rp = w.wcs_world2pix([[fiber_ra[0]+fiber_dia,fiber_dec[0]]],1)
     rp -= w.wcs_world2pix([[fiber_ra[0],fiber_dec[0]]],1)
-    rp = 1 # TODO fix this!!!
+    rp = 5 # TODO fix this!!!
 
     # we'll take a sum over each fiber's pixels.
-    fibsum = np.zeros(pixcrd.shape[0])
+    fibsum = np.zeros(pixcrd.shape[0],dtype='float32')
     if errors:
-        fiberr = np.zeros(pixcrd.shape[0])
+        fiberr = np.zeros(pixcrd.shape[0],dtype='float32')
 
-    zoomed_img = zoom(input=img_data,zoom=zoom_factor,order=1)
+    # we're going to find the region of the image we want to sum over for each fiber
+    # and 'zoom' into it (i.e., make a 3x3 region a 3*zoom_factor x 3*zoom_factor region,
+    #   interpolating to fill in the gaps)
+    # 'zoom'-ing like this reduces the error from summing square pixels in a circular region
     effr = zoom_factor*rp-0.5
-    nx,ny = zoomed_img.shape
+
+    rad_to_zoom = rp+3 # keep a few extra pixels for when we interpolate
+    dzoom = rad_to_zoom*zoom_factor
+    y,x = np.ogrid[-dzoom:dzoom,-dzoom:dzoom]
+    mask = x*x+y*y <= effr*effr
+    errmask = x*x+y*y <= rp*rp*zoom_factor**2
 
     for i in range(pixcrd.shape[0]):
-        a,b = zoom_factor*pixcrd[i]
-        # take a circular mask around the fiber's center
-        y,x = np.ogrid[-a:nx-a,-b:ny-b]
-        mask = x*x+y*y <= effr*effr
-        fibsum[i] = np.sum(zoomed_img[mask])/(zoom_factor**2)
+        a,b = pixcrd[i]
+        # zoom in to the cropped region around our fiber
+        #plt.imshow(img_data[int(a-rad_to_zoom):int(a+rad_to_zoom),int(b-rad_to_zoom):int(b+rad_to_zoom)],interpolation='nearest',cmap='Greys_r',origin='lower')
+        #plt.show()
+        zoomedregion=zoom(input=img_data[int(a-rad_to_zoom):int(a+rad_to_zoom),int(b-rad_to_zoom):int(b+rad_to_zoom)]
+                ,zoom=zoom_factor,order=3)
+        #plt.imshow(zoomedregion,interpolation='nearest',cmap='Greys_r',origin='lower')
+        #plt.show()
+        fibsum[i] = np.sum(zoomedregion[mask])/(zoom_factor**2)
         if errors:
-            fiberr[i] = np.abs(fibsum[i] - np.sum(zoomed_img[x*x+y*y <= rp*rp*zoom_factor**2])/zoom_factor**2)
-            # this error is roughly the possible missed flux from pixels on the edge
+            fiberr[i] = np.abs(fibsum[i] - np.sum(zoomedregion[errmask])/zoom_factor**2)
+            # this error is roughly the possible missed flux from approximating the circle
     if errors:
         return fibsum,fiberr
     else: return fibsum
@@ -59,4 +71,4 @@ if __name__ == '__main__':
             fiber_ra.append(float(a))
             fiber_dec.append(float(b))
     print("Flux at the first 30 fibers: ")
-    print(get_flux("imaging/frame-g-002326-3-0078.fits",fiber_ra,fiber_dec,fiber_dia,zoom_factor=1,errors=False))
+    print(get_flux("imaging/frame-g-002326-3-0078.fits",fiber_ra[:30],fiber_dec[:30],fiber_dia,zoom_factor=2.0,errors=False))
