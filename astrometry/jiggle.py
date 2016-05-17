3838b76bc9f406f4fbb23036a3d9466dab18b203
Maria Jose Bustamante Rosell <majoburo@login2.maverick.tacc.utexas.edu>
2016-05-17 15:41:23 -0500

    rough fist version

--- astrometry/jiggle.py
+++ astrometry/jiggle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
+import sys
+import os
+import glob
+
 import pyfits
 import numpy as np
 import matplotlib.pyplot as plt
 from optparse import OptionParser
-import h5py
-from integrate import get_flux
-import sys
-import os
-import glob
 import os.path as op
 from scipy import interpolate
 from astropy import wcs
@@ -19,36 +18,50 @@ sys.path.append(ppath)
 
 sdss_fits_fname = 'imaging/frame-g-002326-3-0078.fits'
 ifuPos = 'offsets/ifuPos075.txt'
-f = open(ifuPos,'r')
-f.readline()
-f.readline()
-ra0 , dec0 = [],[]
-for line in f:
-    a,b,diameter,throughput = line.split()
-    ra0.append(float(a))
-    dec0.append(float(b))
+pixCrd = 'offsets/pixCrd.txt'
+os.environ['YODASRC'] = "/work/03237/majoburo/maverick/yoda/src"
+
+def photometry():
+    cmd = "$YODASRC/yoda -P --no-kron-ap -p image.phot -M %s -A 1.89 -z 0  %s  &> /dev/null" % (pixCrd,sdss_fits_fname)
+    #print "> " + cmd
+    os.system(cmd)
+
+    dssifu=[]
+    phot=np.loadtxt('image.phot')
+    dssifu=[[atribute[12],atribute[13]] for atribute in phot]
+    return dssifu
+
+def getifuPos(ifuPos):
+
+    f = np.loadtxt(ifuPos)
+    positions=[f[:,0].tolist(),f[:,1].tolist()]
+    return  positions
 
 
-
-
-def chi2(vifu,dssifu,evifu=1,edssifu=1):
+def findchi2(vifu,dssifu,evifu=1,edssifu=1):
 # Evaluate chi-squared.
     chi2 = 0.0
     for n in range(len(vifu)):
-        residual = (vifu[n] - dssifu[n])/(evifu[n]*edssifu[n]) #is this right???
+        #residual = (vifu[n] - dssifu[n])/(evifu[n]*edssifu[n]) #is this right???
+        residual = vifu[n] - dssifu[n] #without errors
         chi2 = chi2 + residual*residual
     
     return chi2
 
-def wcs2pix(sdss_fits_fname,fiber_ra,fiber_dec):
+def wcs2pix(fiber_ra,fiber_dec,fitsfile=sdss_fits_fname):
 
-    with fits.open(sdss_fits_fname) as h:
+    with fits.open(fitsfile) as h:
         img_data = h[0].data
         w = wcs.WCS(h[0].header)
     pixcrd = w.wcs_world2pix(np.array(zip(fiber_ra,fiber_dec),dtype='float64'),1)
-    np.savetxt("pixcrd.txt",pixcrd)
+    pixcrd = [[i+1,pixcrd[i].tolist()[0],pixcrd[i].tolist()[1]] for i in range(len(pixcrd[:,0]))]
+    np.savetxt(pixCrd,pixcrd)
 
-def optimizelin(vifu,dssifu):
+    return
+
+
+
+def jiggle(positions,virus_flux,steps = 5,ddec=0.01,dra=0.01):
 
    # Shots will be jiggled over a range of steps
    # with 5 steps : (where x marks the shot center)
@@ -58,19 +71,33 @@ def optimizelin(vifu,dssifu):
    # o o x o o -> and so on...
    # o o o o o ->
    # o o o o o ->
-   steps = 5
-   ddec=0.01
-   dra=0.01
-   theta=dec0-ddec*steps/2 #min value of theta to scan
-   for s1 in range(steps):
-       theta=theta+ddec #step in theta
-       phi=ra0-dra*steps/2 #min value of phi to scan
-       for s2 in range(steps):
-           phi=phi+dra #step in phi
-           print theta,phi
-           dssifu,edssifu = get_flux(sdss_fits_fname,phi,theta,1,1) #calling function that given an ra and dec will give u flux in sloan image centered there
-           chi2list = [chi2(vifu,dssifu),phi,theta]
-       chi2,phi,theta=np.amin(chi2list,0)
+    chi2min=0
+    ra0=np.array(positions[0])
+    dec0=np.array(positions[1])
+
+    dectemp=dec0-ddec*steps/2 #min value of dec to scan
+    for s1 in range(steps):
+        ratemp=ra0-dra*steps/2 #min value of ra to scan
+        for s2 in range(steps):
+        #MAIN ROUTINE
+            wcs2pix(ratemp,dectemp)
+            dssifu = photometry()
+            dss_flux = np.array(dssifu)[:,0]
+
+            chi2 = findchi2(virus_flux,dss_flux)
+
+            if (chi2min == 0):
+                chi2min = [chi2,ratemp,dectemp,dss_flux]
+            elif (chi2min[0] > chi2):
+                chi2min = [chi2,ratemp,dectemp,dss_flux]
+           
+            ratemp=ratemp+dra #step in ra
+        dectemp=dectemp+ddec #step in theta
+          
+
+    #dssifu = get_flux(sdss_fits_fname,phi,theta,1,1) #calling function that given an ra and dec will give u flux in sloan image centered there
+    #chi2list = [chi2(vifu,dssifu),phi,theta]
+    #chi2,phi,theta=np.amin(chi2list,0)
     #optimizelin()
     #with  phi theta as new centers repeat
     #optimizelin()
@@ -79,11 +106,7 @@ def optimizelin(vifu,dssifu):
     #def linfun(x,a,b):
     #    return a+b*x
     #optimization.curve_fit(linfun, vifu, ifu,x0, sigma)
-
-
-
-    
-
+    return chi2min
 
 def weigthspectra(data,x):
     #make a weight based on some filter file and return filter spectra
@@ -171,28 +194,21 @@ def wavelenghtrange(hdulist):
 def main():
 
     fitsfiles = parseargs()
-    '''
+    
     wl = wavelenghtrange(fitsfiles[0])
     
     'Handeling spectral data from IFU'
     data=[]
     data = [spectra for fitsfile in fitsfiles for spectra in fitsfile.data]
     wdata = weigthspectra(data,wl)
-    vifu = [x.sum() for x in data]
-   '''
+    virus_flux = [x.sum() for x in data]
 
     'Handeling imaging data'
-    #getsdssimage()
-
-    with open(ifuPos,'r') as f:
-        fiber_ra , fiber_dec = [],[]
-        for line in f:
-            a,b,c,d = line.split()
-            fiber_ra.append(float(a))
-            fiber_dec.append(float(b))
-    wcs2pix(sdss_fits_fname,fiber_ra,fiber_dec)
-
-    #print wdata.shape,len(vifu)
-
+  #  getsdssimage()
+    positions = getifuPos(ifuPos)
+   #   for i in range(4)
+    dss_flux = jiggle(positions,virus_flux)[3]
+    plt.plot(virus_flux,dss_flux)
+    plt.show()
 if __name__ == "__main__":
     main()
